4a3f8efc429bf023fc58c37284474b37ffb44e9b
HajarElMannani <laso125@hotmail.com>
2025-05-26 22:30:45 +0100

    Pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,11 +3,7 @@
 Manage the API authentication
 """
 from typing import List, TypeVar
-from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
 
 
 class Auth():
@@ -16,11 +12,9 @@ class Auth():
         ''' that returns False'''
         return False
 
-
-    def authorization_header(self, request=None) -> str: 
+    def authorization_header(self, request=None) -> str:
         '''returns None'''
         return None
-
 
     def current_user(self, request=None) -> TypeVar:
         '''returns None'''

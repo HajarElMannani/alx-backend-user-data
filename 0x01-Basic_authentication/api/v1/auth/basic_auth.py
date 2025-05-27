359e8471f3466ac574b57d28e4df5bb5d1bded08
HajarElMannani <laso125@hotmail.com>
2025-05-27 12:39:53 +0100

    Add basic_auth.py

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Manage the API authentication
+"""
+from typing import List, TypeVar
+from flask import Flask, jsonify, abort, request
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    '''Empty class'''
+    pass

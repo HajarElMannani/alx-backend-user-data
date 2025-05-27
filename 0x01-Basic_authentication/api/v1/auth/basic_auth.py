39acf7474154d4675ff20a4e3f1ac9c296991460
HajarElMannani <laso125@hotmail.com>
2025-05-27 16:40:01 +0100

    Add method user_object_from_credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,7 @@ from flask import Flask, jsonify, abort, request
 from api.v1.auth.auth import Auth
 import base64
 import binascii
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -47,3 +48,21 @@ class BasicAuth(Auth):
             return None, None
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        '''Method that returns the User instance based
+        on his email and password'''
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            user_inst = User.search({'email': user_email})
+        except Exception:
+            return None
+        if user_inst is None or user_inst == []:
+            return None
+        if user_inst[0].is_valid_password(user_pwd) is False:
+            return None
+        return user_inst[0]

8e853d16d88d75b13e88caae9e355b3ae6c6d1b9
HajarElMannani <laso125@hotmail.com>
2025-05-28 18:40:18 +0100

    Add route fot login

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Module of session authenticaton  views
+"""
+from api.v1.views import app_views
+from flask import jsonify, request, make_response
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login_session():
+    '''session authentication'''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None:
+        return jsonify({"error": "email missing"}), 400
+    if password is None:
+        return jsonify({"error": "password missing"}), 400
+    try:
+        user = User.search({'email': email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not user or user == []:
+        return jsonify({"error": "no user found for this email"}), 404
+    if user[0].is_valid_password(password) is False:
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user[0].id)
+    resp = make_response(jsonify(user[0].to_json()))
+    resp.set_cookie(getenv('SESSION_NAME'), session_id)
+    return resp

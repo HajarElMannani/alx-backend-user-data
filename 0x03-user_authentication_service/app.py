bbffe7a83b7d432a702c49f9ae162f908c08365f
HajarElMannani <laso125@hotmail.com>
2025-06-02 20:52:46 +0100

    Add routr /users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 '''basic Flask app'''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
-
+AUTH = Auth()
 
 @app.route('/', methods=['GET'])
 def basic() -> None:
     '''return hello message'''
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user():
+    '''user registation end-point'''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":

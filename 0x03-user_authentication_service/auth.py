47e9b2bb6ca7e5f59370663289d2d19388bf2b31
HajarElMannani <laso125@hotmail.com>
2025-06-04 10:23:19 +0100

    add get_reset_password_token method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,3 +70,13 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """Destroy the user seesion"""
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate reset password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token

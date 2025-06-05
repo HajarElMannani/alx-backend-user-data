1114c3e4dba443521d3af65dc3e085ac8bb7b416
HajarElMannani <laso125@hotmail.com>
2025-06-05 22:22:33 +0100

    Add main module

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-from auth import Auth
+"""main module to start the app"""
+import requests
 
-email = 'me@me.com'
-password = 'mySecuredPwd'
 
-auth = Auth()
+def register_user(email: str, password: str) -> None:
+    """register the user"""
+    resp = requests.post("http://0.0.0.0:5000/users",
+                         data={"email": email,
+                               "password": password})
+    assert resp.status_code == 200 or resp.status_code == 400
+    if resp.status_code == 200:
+        assert resp.json() == {"email": email, "message": "user registered"}
+    elif resp.status_code == 400:
+        assert resp.json() == {"message": "user already registered"}
 
-try:
-    user = auth.register_user(email, password)
-    print("successfully created a new user!")
-except ValueError as err:
-    print("could not create a new user: {}".format(err))
 
-try:
-    user = auth.register_user(email, password)
-    print("successfully created a new user!")
-except ValueError as err:
-    print("could not create a new user: {}".format(err)) 
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
